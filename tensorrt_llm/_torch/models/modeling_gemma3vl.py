b15d2efb2467803ac3c5aef91465c2a70be07089
Balaram Buddharaju <169953907+brb-nv@users.noreply.github.com>
2025-07-08 01:45:45 +0000

    save intermediate changes

--- tensorrt_llm/_torch/models/modeling_gemma3vl.py
+++ tensorrt_llm/_torch/models/modeling_gemma3vl.py
@@ -105,48 +105,6 @@ class Gemma3InputProcessor(InputProcessor):
         }
 
 
-def get_gemma3_causal_mask(
-    input_ids: torch.Tensor,
-    image_token_index: int,
-    sliding_window: Optional[int] = None,
-):
-    print("[get_gemma3_causal_mask] input_ids: ", input_ids)
-    assert input_ids.ndim == 1, "input_ids should be a 1D tensor."
-    # Get token type ids. 0 corresponds to text tokens, 1 corresponds to image tokens.
-    token_type_ids = torch.zeros_like(input_ids, device=input_ids.device)
-    image_token_mask = (input_ids == image_token_index).to(
-        device=input_ids.device, dtype=torch.bool)
-    token_type_ids[image_token_mask] = 1
-
-    sequence_length = input_ids.shape[-1]
-    # TODO: Use causal when sliding_window is larger than sequence_length.
-    if sliding_window is None:
-        causal_mask = torch.arange(
-            sequence_length,
-            device=input_ids.device).unsqueeze(0) <= torch.arange(
-                sequence_length, device=input_ids.device).unsqueeze(1)
-    else:
-        attention_mask_1 = torch.arange(
-            sequence_length,
-            device=input_ids.device).unsqueeze(0) <= torch.arange(
-                sequence_length, device=input_ids.device).unsqueeze(1)
-        attention_mask_2 = torch.arange(
-            sequence_length,
-            device=input_ids.device).unsqueeze(0) > torch.arange(
-                sequence_length,
-                device=input_ids.device).unsqueeze(1) - sliding_window
-        causal_mask = attention_mask_1 & attention_mask_2
-
-    # Apply a bidirectional mask for image tokens.
-    if token_type_ids is not None:
-        token_type_mask = token_type_ids.unsqueeze(
-            0) == token_type_ids.unsqueeze(1)
-        # If text token, do not change anything.
-        token_type_mask[token_type_ids == 0] = False
-        causal_mask = causal_mask.masked_fill(token_type_mask, True)
-    return causal_mask
-
-
 @register_auto_model("Gemma3ForConditionalGeneration")
 @register_input_processor(Gemma3InputProcessor, model_type="gemma3")
 class Gemma3Model(PreTrainedModel):
@@ -189,6 +147,40 @@ class Gemma3Model(PreTrainedModel):
     def infer_max_seq_len(self) -> int:
         return self.llm.infer_max_seq_len()
 
+    def get_gemma3_causal_mask(
+        self,
+        token_type_ids: torch.Tensor,
+        image_token_index: int,
+        sliding_window: Optional[int] = None,
+    ):
+        device = token_type_ids.device
+        sequence_length = len(token_type_ids)
+        # TODO: Use causal when sliding_window is larger than sequence_length.
+        if sliding_window is None:
+            causal_mask = torch.arange(
+                sequence_length,
+                device=device).unsqueeze(0) <= torch.arange(
+                    sequence_length, device=device).unsqueeze(1)
+        else:
+            attention_mask_1 = torch.arange(
+                sequence_length,
+                device=device).unsqueeze(0) <= torch.arange(
+                    sequence_length, device=device).unsqueeze(1)
+            attention_mask_2 = torch.arange(
+                sequence_length,
+                device=device).unsqueeze(0) > torch.arange(
+                    sequence_length, device=device).unsqueeze(1) - sliding_window
+            causal_mask = attention_mask_1 & attention_mask_2
+
+        # Apply a bidirectional mask for image tokens.
+        if token_type_ids is not None:
+            token_type_mask = token_type_ids.unsqueeze(
+                0) == token_type_ids.unsqueeze(1)
+            # If text token, do not change anything.
+            token_type_mask[token_type_ids == 0] = False
+            causal_mask = causal_mask.masked_fill(token_type_mask, True)
+        return causal_mask
+
     @torch.inference_mode()
     def forward(
         self,
@@ -209,33 +201,26 @@ class Gemma3Model(PreTrainedModel):
             mm_embed
         ) == num_context_requests, "Number of multimodal features (if provided) should be equal to number of context requests"
 
-        # Currently, we supply global and local attention masks to the model only when
-        # there are image tokens - specifically in the context phase.
-        global_attention_mask = None
-        local_attention_mask = None
-        if len(mm_embed) != 0:
-            # Request has image tokens.
-            global_attention_mask = get_gemma3_causal_mask(
-                input_ids=input_ids, image_token_index=self.image_token_index)
-            local_attention_mask = get_gemma3_causal_mask(
-                input_ids=input_ids,
-                image_token_index=self.image_token_index,
-                sliding_window=self.sliding_window,
-            )
+        mm_token_ids = torch.tensor([self.image_token_index]).to(input_ids.device)
+        text_token_mask = None
+        if len(mm_embed) > 0:
+            # Get token type ids. 0 corresponds to text tokens, 1 corresponds to image tokens.
+            mm_token_mask = torch.isin(input_ids, mm_token_ids)
+            text_token_mask = ~mm_token_mask
         input_ids, inputs_embeds = fuse_input_embeds(
             embedding_layer=self.llm.model.embed_tokens,
             input_ids=input_ids,
             mm_embeds=mm_embed,
-            mm_token_ids=torch.tensor([self.image_token_index
-                                       ]).to(input_ids.device))
+            mm_token_ids=mm_token_ids
+        )
         logits = self.llm.forward(
             attn_metadata=attn_metadata,
             input_ids=input_ids,
             position_ids=position_ids,
             inputs_embeds=inputs_embeds,
             return_context_logits=return_context_logits,
-            global_attention_mask_data=global_attention_mask,
-            local_attention_mask_data=local_attention_mask,
+            # TODO: Pass this down.
+            #text_token_mask=text_token_mask,
         )
         return logits
 
