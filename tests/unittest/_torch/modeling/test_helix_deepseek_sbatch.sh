276e9835fe1afdb17ea2c37abeef9ba2519f8d1b
Matthias Jouanneaux <mjoux@nvidia.com>
2025-10-15 13:08:04 +0200

    helix single layer: fixed test, added config submit script, improved parsing
    Signed-off-by: Matthias Jouanneaux <mjoux@nvidia.com>

--- tests/unittest/_torch/modeling/test_helix_deepseek_sbatch.sh
+++ tests/unittest/_torch/modeling/test_helix_deepseek_sbatch.sh
@@ -35,9 +35,9 @@ set -x
 
 export WORKDIR="/lustre/fsw/portfolios/coreai/projects/coreai_horizon_dilations/users/mjoux/helix"
 export CONTAINER_NAME="tllm_pyt2508_py3_aarch64_trt10.13.2.6_202509112230_7568"
-export CONTAINER_IMAGE="/lustre/fsw/portfolios/coreai/projects/coreai_horizon_dilations/users/mjoux/containers/tllm_pyt2508_py3_aarch64_trt10.13.2.6_202509112230_7568.sqsh"
+export CONTAINER_IMAGE="/lustre/fsw/portfolios/coreai/projects/coreai_horizon_dilations/users/mjoux/containers/tllm_pyt2508_py3_aarch64_trt10.13.2.6_202509112230_7568_build7e31a9b56d10e83fe1e654dbccc10e7e5bbad0f0.sqsh"
 export CONTAINER_MOUNT="/lustre/:/lustre/"
-logdir=\${WORKDIR}/slurm-\${SLURM_JOB_ID}-N${NODES}
+logdir=\${WORKDIR}/slurm-\${SLURM_JOB_ID}
 mkdir -p \${logdir}
 full_logdir=\${logdir}
 trtllm_repo=/lustre/fsw/portfolios/coreai/projects/coreai_horizon_dilations/users/mjoux/TensorRT-LLM
@@ -49,32 +49,6 @@ if ! srun -l --container-image=\${CONTAINER_IMAGE} \
         --mpi=pmix \
         echo "Container up." &> \${full_logdir}/container_launch.log; then
     cleanup_on_failure "Failed to start container. Check \${full_logdir}/container_launch.log"
-fi
-
-if [ -d "\${trtllm_repo}" ]; then
-    echo "Installing TensorRT-LLM from \${trtllm_repo}..."
-    TRT_LLM_GIT_COMMIT=\$(git -C \${trtllm_repo} rev-parse --short HEAD 2>/dev/null || echo "unknown")
-    echo "TRT_LLM_GIT_COMMIT: \${TRT_LLM_GIT_COMMIT}"
-
-    #echo "Building TensorRT-LLM wheel on one node..."
-    #build_command="python3 \${trtllm_repo}/scripts/build_wheel.py --trt_root /usr/local/tensorrt --benchmarks --use_ccache --cuda_architectures \"\${cuda_architectures}\""
-    #if ! srun --container-name=\${CONTAINER_NAME} \
-    #    --container-mounts=\${CONTAINER_MOUNT} \
-    #    --mpi=pmix --overlap -N 1 --ntasks-per-node=1 \
-    #    bash -c "cd \${trtllm_repo} && \${build_command}" \
-    #    &> \${full_logdir}/build.log; then
-    #    cleanup_on_failure "TensorRT-LLM build failed. Check \${full_logdir}/build.log for details"
-    #fi
-
-    echo "Installing TensorRT-LLM..."
-    if ! srun --container-name=\${CONTAINER_NAME} \
-        --container-mounts=\${CONTAINER_MOUNT} \
-        --mpi=pmix --overlap -N \$SLURM_NNODES --ntasks-per-node=1 \
-        bash -c "cd \${trtllm_repo} && pip install -e ." \
-        &> \${full_logdir}/install.log; then
-        cleanup_on_failure "TensorRT-LLM installation failed. Check \${full_logdir}/install.log for details"
-    fi
-    echo "TensorRT-LLM installation completed successfully"
 fi
 
 export TLLM_LOG_LEVEL="INFO" # DEBUG is verbose
